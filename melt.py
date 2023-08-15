1aa633b8db3d62dab121fb37c3dbfb01516b3289
acchu <paru@gmail.com>
2023-08-16 04:39:12 +0530

    change1

---
+++ melt.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+a=pd.DataFrame({'banglore':[12,43,56],
+                'tumkuru':[12,35,78],
+                'mysuru':[34,67,89]})
+print(a)
+print(a.melt())
+
+
+
+
+
