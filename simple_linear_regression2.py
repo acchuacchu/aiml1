14f6e030c7588120f5e52f87f61cd770bd54008a
acchu <paru@gmail.com>
2023-09-12 12:44:15 +0530

    change10

---
+++ simple_linear_regression2.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+data=pd.read_csv("C:/Users/SPTINT-13/Desktop/dataset/CIE_SEE dataset.csv")
+print(data)
+print(data.isna().sum())
+x=data['cie'].values.reshape(-1,1)
+print(x)
+y=data['see'].values.reshape(-1,1)
+print(y)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=4)
+print(x_train)
+print(x_test)   
+print(y_train)
+print(y_test)
+
+from sklearn.linear_model import LinearRegression
+
+m=LinearRegression()
+print(m.fit(x_train,y_train))
+pre=m.predict(x_test)
+print(pre)
+
